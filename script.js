39b9ff59485a58072f38ce1d7c9e09b85fa19922
ScripterGo <48377932+ScripterGo@users.noreply.github.com>
2021-04-15 09:53:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 
+function go_home(){
+    location.href = "index.html";
+}
+function go_about(){
+    location.href = "#";
+}
+function go_list(){
+    location.href = "#";
+}
+function go_explorer(){
+    location.href = "explorer.html";
+}
+function go_login(){
+    location.href = "#";
+}
+
+window.onload = function(){
+    let collection = document.getElementsByClassName("lower_nav_btn");
+    console.log(collection);
+    for(let i = 0; i < collection.length; i++){
+        console.log(collection[i]);
+    }
+};
 
